0fd19392cba04a5f799f3c8d4e31f87b78580a46
Chirag Soni <chirag22soni04@gmail.com>
2023-03-28 17:10:09 +0530

    28-03-2023 chirag key board issue solved search fragment

--- app/src/main/java/com/infovass/catering/activities/home/view/SearchFragment.java
+++ app/src/main/java/com/infovass/catering/activities/home/view/SearchFragment.java
@@ -184,6 +184,7 @@ public class SearchFragment extends Fragment implements RestourentView {
                         .putExtra("restaurant_Status", restaurant_Status);
                 startActivity(intent);
                 activity.overridePendingTransition(R.anim.left_slide_in, R.anim.right_slide_out);
+                hideKeyboard(activity);
 
             }
         });
@@ -234,7 +235,16 @@ public class SearchFragment extends Fragment implements RestourentView {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
     }
-
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
     public void closeKeyboard() {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
